aa2a152a9661b0a164b3646e32e8e3c67ba14d18
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-09-09 14:40:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", {id: "parent"}, 
+    React.createElement("div", {id: "child"}, [
+        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
+        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
+    ],
+    React.createElement("div", {id: "child"}, [
+        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
+        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
+    ]))
+);
+
+
+// const heading = React.createElement("h1", {id: "heading" , xyz:"abc"} , "hello world in react");
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
