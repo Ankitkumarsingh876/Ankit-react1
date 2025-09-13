9594406c59ceb52d57fc8ae2fb1bae0f27cd0850
Ankit Kumar Singh <singhankittomar.23@gmail.com>
2025-09-13 11:14:20 +0530

    First commit

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
-const parent = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"}, [
-        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
-        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
-    ],
-    React.createElement("div", {id: "child"}, [
-        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
-        React.createElement("div", {id: "parent"}, "hi i am h1 tag"),
-    ]))
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-
-// const heading = React.createElement("h1", {id: "heading" , xyz:"abc"} , "hello world in react");
-
-console.log(parent);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     [
+//       React.createElement("div", { key: 1, className: "item" }, "hi i am ankit kumar singh"),
+//       React.createElement("div", { key: 2, className: "item" }, "hi I am 2 tag"),
+//     ]
+//   ),
+//   React.createElement(
+//     "div",
+//     { id: "child2" },
+//     [
+//       React.createElement("div", { key: 3, className: "item" }, "hi I ajgsjfsjfsjffjjgljgsgnksdfngkg tag"),
+//       React.createElement("div", { key: 4, className: "item" }, "hi I sdglkhdskghsklghshgkldfgjsdfgjklam h1 tag"),
+//     ]
+//   )
+// );
+const JsxHeading = <h1 className="heading" tabIndex="2">
+  hi i am ankit kumar singh
+  </h1>;
+console.log(JsxHeading);
+// console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(JsxHeading);
